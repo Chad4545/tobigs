3fe8f1688eca99fb7210c4de3c9317349b7c8af5
Chad4545 <genom1324@gmail.com>
2019-02-14 23:35:22 +0900

    TODO

---
+++ Algorithm/4wk_algorithm_edit/student.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+class Student:
+    def __init__(self, id=None, name=None):
+        self.id = id
+        self.name = name
+        
+    def __str__(self):
+        pass
+    
+    def __lt__(self):
+        pass
+
