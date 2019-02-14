097e4a09b8c0d3db0b93e529760ad8db71a9ca4c
Chad4545 <genom1324@gmail.com>
2019-02-14 22:48:07 +0900

    TODO

---
+++ Algorithm/4wk_algorithm_edit/Ban.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+class Ban:
+    def __init__(self, no=None):
+        self.no = no
+        self.student_list =[]
+    
+    def __str__(self):
+        pass
+    
+    def __lt__(self, other):
+        pass
+    
+    def __eq__(self, other):
+        pass
+    
+    def count_student(self):
+        """해당 반에 속해있는 학생들의 수 return"""
+
